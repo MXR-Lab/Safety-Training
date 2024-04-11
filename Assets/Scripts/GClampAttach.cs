eca6c297d1bcb9a41ed4db2d71b263b46327c728
Npfant <npfant@gmail.com>
2024-04-11 12:30:09 -0500

    G Clamp Attach Points Added

---
+++ Assets/Scripts/GClampAttach.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GClampAttach : MonoBehaviour
+{
+    [SerializeField] GameObject obj;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.Equals(obj.GetComponent<Collider>()))
+        {
+            obj.transform.parent.position = transform.position;
+            obj.transform.parent.rotation = transform.rotation;
+            obj.GetComponent<Rigidbody>().useGravity = false;
+            obj.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.Equals(obj.GetComponent<Collider>()))
+        {
+            obj.GetComponent<Rigidbody>().useGravity = true;
+            obj.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}

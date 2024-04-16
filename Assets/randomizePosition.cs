76e6f2aa4e4eca0ecd1e491ea7d40d9f0689fc61
Npfant <npfant@gmail.com>
2024-04-15 23:06:47 -0500

    Added shirt folding script

--- Assets/randomizePosition.cs
+++ Assets/randomizePosition.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class randomizePosition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Transform trans = transform.position;
 }

0b6c457a4caecbb5118878716aa10ee63add01da
Npfant <npfant@gmail.com>
2024-04-16 22:06:07 -0500

    Added random object placement script

--- Assets/randomizePosition.cs
+++ Assets/randomizePosition.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class randomizePosition : MonoBehaviour
 {
-    Transform trans = transform.position;
+    public GameObject prefab;
+    int xPos; int zPos;
+    int index = 0;
+    [SerializeField] int objectToAdd;
+
+    void Start() {
+        while (index < objectToAdd)
+        {
+            xPos = Random.Range(-5,5);
+            zPos = Random.Range(-5,5);
+            Instantiate(prefab, gameObject.transform.TransformPoint(new Vector3(xPos, 1/2, zPos)), gameObject.transform.rotation);
+            index++;
+        }
+    }
+
 }

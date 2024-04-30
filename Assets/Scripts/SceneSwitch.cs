c68dd84446d5bf5d1ad96ddf7e45602bdb7674ca
Npfant <npfant@gmail.com>
2024-04-30 18:22:55 -0500

    Periodic save

--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -5,14 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
-    //GameObject Scene
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     public void LoadElevated() {
         //sceneManager.UnloadSceneAsync(sceneManager.GetActiveScene());
         SceneManager.LoadScene("SampleSceneElevated", LoadSceneMode.Single);
